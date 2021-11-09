e22c3a29dddaa787e56c4fbc73dcd3da87a37093
florianL21 <flo.laschober@gmail.com>
2021-11-09 23:32:13 +0100

    Python path in sphinx config

--- conf.py
+++ conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../Cosmos/customBox/python"))
+sys.path.insert(0, os.path.abspath("../customBox/python"))
 
 
 # -- Project information -----------------------------------------------------
